38a90b5eab594ed74975d4f8d9aa97bef06ab4d4
Terry Song <Terry Song>
2016-10-23 20:07:57 -0400

    added menu and game-over scene, with level manager

---
+++ Shoot-Hoop/Assets/Scripts/LeverManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LeverManager : MonoBehaviour {
+
+    private int current_index;
+
+	// Use this for initialization
+	void Start () {
+        // Load scene
+        current_index = SceneManager.GetActiveScene().buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadNextScene();
+        }
+	}
+
+    void LoadNextScene()
+    {
+        // Load currentIndex + 1
+        SceneManager.LoadScene(current_index + 1);
+    }
+}
