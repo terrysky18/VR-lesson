5bd2e68fad5eed24ffe927503c67b3f09a515ac9
Terry Song <Terry Song>
2016-11-03 17:25:28 -0400

    added timer in game scene

--- Shoot-Hoop/Assets/Scripts/LeverManager.cs
+++ Shoot-Hoop/Assets/Scripts/LeverManager.cs
@@ -3,34 +3,73 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class LeverManager : MonoBehaviour {
+    public float timeTillNextLevel;
 
-    private float timeTillNextLevel = 5f;
-
-    private int current_index;
-
-	// Use this for initialization
-	void Start () {
+    private int current_scene_index;
+    private bool auto_progress;
+    // Use this for initialization
+    void Start () {
         // Load scene
-        current_index = SceneManager.GetActiveScene().buildIndex;
+        current_scene_index = SceneManager.GetActiveScene().buildIndex;
+        auto_progress = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (auto_progress)
         {
-            LoadNextScene();
+            CycleScenes();
         }
-        timeTillNextLevel -= Time.deltaTime;
-        Debug.Log("Time until next level:  " + timeTillNextLevel);
-        if(timeTillNextLevel <= 0)
+        else
         {
-            LoadNextScene();
+            // only use clock counterdown in game scene
+            if (current_scene_index == 1)
+            {
+                timeTillNextLevel -= Time.deltaTime;
+                Debug.Log("current level index:  " + current_scene_index);
+                Debug.Log("Time to next scene:  " + timeTillNextLevel);
+                if (timeTillNextLevel <= 0)
+                {
+                    // time is up
+
+                    LoadNextScene();
+                }
+            }
         }
 	}
+
+    void CycleScenes()
+    {
+        // Cycles through all in the scenes in Build Settings
+        // useful debug function
+        timeTillNextLevel -= Time.deltaTime;
+        // check total number of scenes
+        int scene_count = SceneManager.sceneCountInBuildSettings;
+        Debug.Log("Auto progress:  " + auto_progress);
+
+        Debug.Log("Time until next level:  " + timeTillNextLevel);
+        //Debug.Log("Total scenes in build setting:  " + SceneManager.sceneCountInBuildSettings);
+
+        if (timeTillNextLevel <= 0)
+        {
+            if (current_scene_index == (scene_count - 1))
+            {
+                // current scene is the last scene
+                // cycle back to first scene
+                current_scene_index = -1;
+            }
+            SceneManager.LoadScene(current_scene_index + 1);
+        }
+    }
 
     public void LoadNextScene()
     {
         // Load currentIndex + 1
-        SceneManager.LoadScene(current_index + 1);
+        SceneManager.LoadScene(current_scene_index + 1);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("02_Game");
     }
 }
